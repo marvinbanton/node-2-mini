06c0531a2f0b501bb08ab9acbf2b2d2ff5b6dac4
marvin banton <marvin.banton@boom.camp>
2019-07-23 11:03:02 +0800

    Done Step 5

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const bc = require('./controllers/booksController.js')
+
 const app = express();
 
 const PORT = 3001;
 
 //Middleware
 app.use(express.json())
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
